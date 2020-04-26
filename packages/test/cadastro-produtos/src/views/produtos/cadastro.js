5d4fc612de215bf05aa18582c917bf5c485bcf20
Hebert P <hebertpktd@gmail.com>
2020-04-26 16:10:36 -0300

    Criação da Navbar e da View Home

---
+++ packages/test/cadastro-produtos/src/views/produtos/cadastro.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class CadastroProduto extends React.component {
+
+    render() {
+        return (
+            <div className="card">
+                <div className="card-header">
+                    
+                    Cadstro de produto
+                </div>
+                <div className="card-body">
+
+                </div>
+            </div> 
+        );
+    }
+}
